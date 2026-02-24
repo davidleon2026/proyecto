e51b159a46737de57ffac1310e3b8a41a13e104b
david.brian.leon <david.brian.leon@estudiantat.upc.edu>
2026-02-24 18:44:01 +0100

    Hola Mundo!

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+def sumar (a,b):
+    """
+    Parameters
+    ----------
+    a: number
+       un sumando
+    b: number
+       otro sumando
+
+    Returns
+    -------
+        la suma de a  y b
+    """
+    return a + b
+
+
+if __name__ == '__main__':
+    print(sumar(2,3))
+
