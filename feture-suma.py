ff1d6437a5126155cc451d8b37cc32dedd67f175
black1599 <david.mora.fernandez@estudiantat.upc.edu>
2026-02-26 11:49:59 +0100

    Prueba 2

--- feture-suma.py
+++ feture-suma.py
@@ -1,1 +1,15 @@
-#hola mundo
+def suma(a,b):
+    """
+    :param a:
+    :param b:
+    :return: a + b
+    >>> suma(2,1)   #doctest para probar funcion
+    3               #resultado que deberia dar doctest
+    """
+    return a+b
+print(suma(1,2))
+#usar doctest en codigo
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
